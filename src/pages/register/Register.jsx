bbde2b545a45bcf9870d78b95158383638ad15a7
NAnastasiyaaa <maherus16@gmail.com>
2022-09-29 21:23:47 +0300

    first commit

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,12 +13,12 @@ function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
+      const res = await axios.post("auth/register", {
         username,
         email,
         password,
       });
-      res.data && window.location.replace("/login");
+      res.data && window.location.replace("login");
     } catch (err) {
       setError(true);
     }
